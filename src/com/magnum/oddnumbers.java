918dd519657b4284605c3c2ac8d2a361a9bdb085
David Sundelius <david.sundelius@gmail.com>
2015-03-14 13:37:44 +0100

    Odd numbers

--- src/com/magnum/oddnumbers.java
+++ src/com/magnum/oddnumbers.java
@@ -1,14 +1,30 @@
 package com.magnum;
 
+//Zorek
+
 public class oddnumbers {
 
   //Denna kod ska minimeras
-  public String runCode(String i) {
-    return "hej" + i;
+  public String runCodeLong(String i) {
+    int j=0,k=0,l=i.length(),m;
+    String s,c="";
+    while(j!=l) {
+      s="";
+      while(j!=l) {
+        c=i.charAt(j)+"";
+        j++;
+        if(c.equals(","))break;
+        s+=c;
+      }
+      if(Integer.valueOf(s)%2==1)k++;
+    }
+    return k+"";
   }
 
+  public String runCode(String i){int j=0,k=0,l=i.length(),m;String s,c="";while(j!=l){s="";while(j!=l){c=i.charAt(j)+"";j++;if(c.equals(","))break;s+=c;}if(Integer.valueOf(s)%2==1)k++;}return k+"";}
+
   public oddnumbers() {
-    System.out.println(runCode("Hello"));
+    System.out.println(runCode("1,2,3,45,5,6,555"));
   }
 
   public static void main(String[] args) {
--- src/com/magnum/samesame.java
+++ src/com/magnum/samesame.java
@@ -1,4 +1,15 @@
 package com.magnum;
 
 public class samesame {
+
+  //Denna kod ska minimera
+  public String runCode(String i) {return i;}
+
+  public samesame() {
+    System.out.println(runCode("Hello"));
+  }
+
+  public static void main(String[] args) {
+    new samesame();
+  }
 }
